54caf64ce20c6ff779fd0d5492a3aff6eeec3145
jimafisk <jimafisk@gmail.com>
2023-01-10 18:21:08 -0500

    Compile svelte file.

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"time"
 
-	"github.com/livebud/js"
-	v8 "github.com/livebud/js/v8"
-	"rogchap.com/v8go"
+	"github.com/test/test/svelte"
 )
-
-type VM struct {
-	isolate *v8go.Isolate
-	context *v8go.Context
-}
 
 func main() {
 	start := time.Now()
-	vm, _ := v8.Load(&js.Console{
-		Log:   os.Stdout,
-		Error: os.Stderr,
-	})
-	defer vm.Close()
-	ctx := context.Background()
-	vm.Evaluate(ctx, "math.js", `const multiply = (a, b) => a * b`)
-	value, _ := vm.Evaluate(ctx, "run.js", "multiply(3, 2)")
-	fmt.Println(value)
+
+	hello, _ := os.ReadFile("hello.svelte")
+	vm, _ := Load()
+	svelteCompiler, _ := svelte.Load(vm)
+	output, _ := svelteCompiler.DOM("hello.svelte", hello)
+	fmt.Println(output.JS)
+
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
 }
