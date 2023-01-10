3e54430f8400b36532ab8ef531985afc1c4b540f
jimafisk <jimafisk@gmail.com>
2023-01-10 18:42:32 -0500

    Basic Plenti site.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	v8 "github.com/livebud/bud/package/js/v8"
@@ -12,11 +13,25 @@ import (
 func main() {
 	start := time.Now()
 
-	hello, _ := os.ReadFile("hello.svelte")
 	vm, _ := v8.Load()
 	svelteCompiler, _ := svelte.Load(vm)
-	output, _ := svelteCompiler.DOM("hello.svelte", hello)
-	fmt.Println(output.JS)
+
+	filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+
+		if !info.IsDir() && filepath.Ext(path) == ".svelte" {
+			fileContents, _ := os.ReadFile(path)
+			DOM, _ := svelteCompiler.DOM(path, fileContents)
+			fmt.Println(DOM.JS)
+			SSR, _ := svelteCompiler.SSR(path, fileContents)
+			fmt.Println(SSR.JS)
+		}
+
+		return nil
+	})
 
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
