0723dba48919a7396e89ac6a12972a39a860c67b
jimafisk <jimafisk@gmail.com>
2023-01-15 20:07:58 -0500

    Debugging esbuild.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/test/test
 go 1.18
 
 require (
-	github.com/evanw/esbuild v0.16.16 // indirect
+	github.com/evanw/esbuild v0.17.0 // indirect
 	github.com/livebud/bud v0.2.5 // indirect
 	github.com/livebud/js v0.0.0-20221112072017-b9e63b92aad5 // indirect
 	go.kuoruan.net/v8go-polyfills v0.5.1-0.20220727011656-c74c5b408ebd // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	esbuild "github.com/evanw/esbuild/pkg/api"
 	v8 "github.com/livebud/bud/package/js/v8"
 	"github.com/livebud/bud/package/svelte"
 )
@@ -24,21 +25,77 @@ func main() {
 			return nil
 		}
 
+		nodes := []string{}
+
 		if !info.IsDir() && filepath.Ext(path) == ".svelte" {
 			fileContents, _ := os.ReadFile(path)
-			destPath := "output" + strings.TrimPrefix(path, "templates")
-			os.MkdirAll(filepath.Dir(destPath), os.ModePerm)
+			destPath := strings.TrimPrefix(path, "templates")
 
+			DOMDest := "js/dom" + strings.TrimSuffix(destPath, ".svelte") + ".js"
+			os.MkdirAll(filepath.Dir(DOMDest), os.ModePerm)
 			DOM, _ := svelteCompiler.DOM(path, fileContents)
-			ioutil.WriteFile(strings.TrimSuffix(destPath, ".svelte")+"_DOM.js", []byte(DOM.JS), os.ModePerm)
+			ioutil.WriteFile(DOMDest, []byte(DOM.JS), os.ModePerm)
 
+			SSRDest := "js/ssr" + strings.TrimSuffix(destPath, ".svelte") + ".js"
+			os.MkdirAll(filepath.Dir(SSRDest), os.ModePerm)
 			SSR, _ := svelteCompiler.SSR(path, fileContents)
-			ioutil.WriteFile(strings.TrimSuffix(destPath, ".svelte")+"_SSR.js", []byte(SSR.JS), os.ModePerm)
+			ioutil.WriteFile(SSRDest, []byte(SSR.JS), os.ModePerm)
+
+			if strings.Contains(destPath, "content") {
+				nodes = append(nodes, SSRDest)
+				//bundle(SSRDest)
+			}
 		}
+		bundle(nodes)
 
 		return nil
 	})
 
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
+}
+
+//func bundle(entrypoint string) {
+func bundle(entrypoints []string) {
+	destPath := "html_output"
+	os.MkdirAll(filepath.Dir(destPath), os.ModePerm)
+	result := esbuild.Build(esbuild.BuildOptions{
+		//EntryPoints: []string{entrypoint},
+		EntryPoints: entrypoints,
+		Bundle:      true,
+		Outdir:      destPath,
+		Plugins:     []esbuild.Plugin{sveltePlugin()},
+	})
+	if len(result.Errors) > 0 {
+		msgs := esbuild.FormatMessages(result.Errors, esbuild.FormatMessagesOptions{
+			Color:         true,
+			Kind:          esbuild.ErrorMessage,
+			TerminalWidth: 80,
+		})
+		fmt.Printf(strings.Join(msgs, "\n"))
+	}
+	if len(result.OutputFiles) > 0 {
+		fmt.Println(string(result.OutputFiles[0].Contents))
+	}
+}
+
+func sveltePlugin() esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "svelte",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(esbuild.OnResolveOptions{Filter: `.*`}, func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+				fmt.Println("RESULT: " + result.Path)
+				fmt.Println("ARGS: " + args.Path)
+				//result.Path = strings.Replace(args.Path, ".svelte", ".js", -1)
+				//result.Namespace = "svelte"
+				return result, nil
+			})
+			epb.OnLoad(esbuild.OnLoadOptions{Filter: `.*`, Namespace: "svelte"}, func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+				//result.ResolveDir = dir
+				//result.Contents = &svelteRuntime
+				result.Loader = esbuild.LoaderJS
+				return result, nil
+			})
+		},
+	}
 }
